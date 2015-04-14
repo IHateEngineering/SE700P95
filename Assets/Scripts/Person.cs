57a3c8b1e13464da27607ff7f100e88b6449b4e9
bo <bwan210@hotmail.com>
2015-04-14 14:21:21 +1200

    basic scripting examples

---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Person : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (teamMemberNumber == 1) {
+//			moveRight ();
+//		}
+	}
+
+	public bool moveRight(){
+		gameObject.transform.Translate(Vector3.right * Time.deltaTime, Space.World);
+		return true;
+	}
+
+	public void moveTo() {
+	
+	}
+
+	public bool moveOtherPersonRightByTag(string tag){
+		GameObject.FindGameObjectWithTag (tag).transform.Translate(Vector3.left * Time.deltaTime, Space.World);
+		return true;
+	}
+
+}
