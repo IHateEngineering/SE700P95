36d5e91eaac5f240a89dc5e795e917c5d4ba894d
bo <bwan210@hotmail.com>
2015-04-29 11:59:43 +1200

    resources folder

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for(int x = 0; x < 5; x++)
+		{
+			for(int y = 0; y < 5; y++)
+			{
+				GameObject obj = (GameObject)Instantiate(Resources.Load ("Prefabs/" + "GridTile"));
+				obj.transform.position = new Vector3(x, 0, y);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
